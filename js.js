9244ad6b4837fcafbae3b458564e1d59610d3b43
heygwangjin <cer3@ajou.ac.kr>
2021-01-13 20:31:06 +0900

    Change 'hi' color to 'hi2'

--- js.js
+++ js.js
@@ -1,4 +1,6 @@
 function myFunction(){
-    var who = document.getElementById('whoru').value;
-    document.getElementById('hi').innerHTML = "Hello "+who+"!";
+    var who = document.getElementById("whoru").value;
+    document.getElementById("hi").innerHTML = "Hello "+who+"!";
+    // document.getElementById("hi").style.backgroundColor="gray";
+    document.getElementById("hi").setAttribute("id", "hi2");
 }
