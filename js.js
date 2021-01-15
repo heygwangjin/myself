781504ef0d0ad04bda41245cee93d86691dea8db
heygwangjin <cer3@ajou.ac.kr>
2021-01-15 17:34:38 +0900

    Add changeColor method

--- js.js
+++ js.js
@@ -4,10 +4,29 @@ function myFunction(){
         document.getElementById("hi").style.display = 'block';
         document.getElementById("hi").innerHTML = "Hello "+who+"!";
         document.getElementById("gwangjin").src= "images/welcome.png"
+        document.getElementById("chcolor2").style.display = 'block';
+        document.getElementById("chcolor").style.display = 'inline';
     }
     else{
         document.getElementById("hi").style.display = 'block';
         document.getElementById("hi").innerHTML = "What's your name?";
         document.getElementById("gwangjin").src= "images/gwangjin.png"
+    }
+}
+function changeColor(){
+    var currentString = document.getElementById("chcolor").value;
+
+    var nextColor = ["#dbffe5", "#eaf5fb", "#e6ddee", "white"];
+    var stringList = ["Next is Mint", "Next is Blue", "Next is Purple", "Next is White"]; 
+    
+    for(i = 0; i < (stringList.length - 1); i++){
+        if (currentString === stringList[3]){
+            document.body.style.backgroundColor = nextColor[3];
+            document.getElementById("chcolor").value = stringList[0];
+        }
+        else (currentString === stringList[i]){
+            document.body.style.backgroundColor = nextColor[i];
+            document.getElementById("chcolor").value = stringList[i + 1];
+        }
     }
 }
